32a8b485b47cb40f5b2bd0e80cbf4db8f3fc57c9
João Pedro <pedrosant1905@gmail.com>
2025-02-05 19:52:35 -0300

    mudança no front

--- src/main/java/com/example/estoque/model/Produto.java
+++ src/main/java/com/example/estoque/model/Produto.java
@@ -1,3 +1,14 @@
+package com.example.estoque.model;
+
+import lombok.Data;
+
+@Data
 public class Produto {
-    
+    private String codigo;
+    private String descricao;
+    private String marca;
+    private double valorEntrada;
+    private double valorSaida;
+    private int quantidadeEstoque;
+    private int vendidos;
 }
