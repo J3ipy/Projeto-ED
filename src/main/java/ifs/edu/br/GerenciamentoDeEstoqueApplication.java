5f223ab7cb779d0303a5a20831d013cf0fcb5b27
João Pedro <pedrosant1905@gmail.com>
2025-02-05 19:21:51 -0300

    pacote do initializer do spring

---
+++ src/main/java/ifs/edu/br/GerenciamentoDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package ifs.edu.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifs/edu/br/GerenciamentoDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package ifs.edu.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
